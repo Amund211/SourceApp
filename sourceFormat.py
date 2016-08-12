cc970c2d8163a54bb1b0853ac9dec2cbae5dda93
Amund211 <amund@svandal.no>
2016-08-12 19:01:23 +0200

    Shit's hard, man...

--- sourceFormat.py
+++ sourceFormat.py
@@ -2,4 +2,27 @@
 def test():
 	pass
 pass
-
+def formatSources(firstName, lastName, pageNumberRange, publishedYear, publicationName, publicationType, publisherName, publisherLocation, language, formatStyle):
+	"""Formats source citations"""
+	#Certain inputs are locked to certain values, and will error if they differ
+	#language, formatStyle, publicationType
+	languages = {}
+	languages["norwegian"] = {"pageShort" : "s."}
+	languages["english"] = {"pageShort" : "p."}
+	
+	#Setting up the formats
+	tmpHarvard = {}
+	tmpHarvardFull["book"] = 
+	tmpHarvardFull["film"] = 
+	tmpHarvardFull["journal"] = 
+	tmpHarvardFull["newspaper"] = 
+	tmpHarvardFull["webpage"] = 
+	
+	#Filling in the formats
+	formats = {"harvard" : {"full" : tmpHarvardFull, "short" : tmpHarvardShort}}
+	
+	
+	#To omit an input set it to a negative number or an empty string, depending on the type
+	#
+	pass
+	
